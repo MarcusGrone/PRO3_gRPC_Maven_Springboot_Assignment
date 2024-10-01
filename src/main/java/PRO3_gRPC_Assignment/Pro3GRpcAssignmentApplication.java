c188bed669e5c5c13a8124c23a61999dfd5dd589
Marcus Grøne <marcusgrone@gmail.com>
2024-10-01 19:52:57 +0200

    First inital push.

---
+++ src/main/java/PRO3_gRPC_Assignment/Pro3GRpcAssignmentApplication.java
@@ -0,0 +1,13 @@
+package PRO3_gRPC_Assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pro3GRpcAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pro3GRpcAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/PRO3_gRPC_Assignment/Pro3GRpcAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package PRO3_gRPC_Assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pro3GRpcAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

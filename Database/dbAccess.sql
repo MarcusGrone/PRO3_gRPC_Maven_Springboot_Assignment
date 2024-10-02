9ef4886a9cc0b78abfefe4814b57f2142f038cb2
Marcus Grøne <marcusgrone@gmail.com>
2024-10-02 20:38:37 +0200

    updated DB

---
+++ Database/dbAccess.sql
@@ -0,0 +1,20 @@
+CREATE USER slaughterhouseserver WITH PASSWORD 'CF(3''(]eA,JOty$WssTf^3';
+
+GRANT SELECT ON ALL TABLES IN SCHEMA pro3_slaughterhouse TO slaughterhouseserver;
+
+GRANT INSERT ON ALL TABLES IN SCHEMA pro3_slaughterhouse TO slaughterhouseserver;
+
+GRANT UPDATE ON ALL TABLES IN SCHEMA pro3_slaughterhouse TO slaughterhouseserver;
+
+GRANT DELETE ON ALL TABLES IN SCHEMA pro3_slaughterhouse TO slaughterhouseserver;
+
+GRANT USAGE ON SCHEMA pro3_slaughterhouse TO slaughterhouseserver;
+
+GRANT USAGE, UPDATE, SELECT ON SEQUENCE pro3_slaughterhouse.animal_animal_id_seq TO slaughterhouseserver;
+GRANT USAGE, UPDATE, SELECT ON SEQUENCE pro3_slaughterhouse.animalpart_part_id_seq TO slaughterhouseserver;
+GRANT USAGE, UPDATE, SELECT ON SEQUENCE pro3_slaughterhouse.parttype_id_seq TO slaughterhouseserver;
+GRANT USAGE, UPDATE, SELECT ON SEQUENCE pro3_slaughterhouse.product_product_id_seq TO slaughterhouseserver;
+GRANT USAGE, UPDATE, SELECT ON SEQUENCE pro3_slaughterhouse.tray_tray_id_seq TO slaughterhouseserver;
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA pro3_slaughterhouse
+GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO slaughterhouseserver;

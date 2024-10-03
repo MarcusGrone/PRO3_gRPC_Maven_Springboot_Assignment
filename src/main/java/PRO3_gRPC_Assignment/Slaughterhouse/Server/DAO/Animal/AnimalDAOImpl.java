a56624c91ffb6514d83072b7b80b7a96fbb4f609
Rasmus Sterup <69890797+Sterup15@users.noreply.github.com>
2024-10-03 09:57:13 +0200

    Added database connection, implemented jdbcTemplate to make SQL queries from DAO

--- src/main/java/PRO3_gRPC_Assignment/Slaughterhouse/Server/DAO/Animal/AnimalDAOImpl.java
+++ src/main/java/PRO3_gRPC_Assignment/Slaughterhouse/Server/DAO/Animal/AnimalDAOImpl.java
@@ -1,5 +1,6 @@
 package PRO3_gRPC_Assignment.Slaughterhouse.Server.DAO.Animal;
 
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -8,9 +9,10 @@ import java.sql.SQLException;
 public class AnimalDAOImpl
 {
 
-    private final JbdcTemplate
+    private final JdbcTemplate jdbcTemplate;
 
-    @Autowired //Todo: find out why JbdcTemplate isn't recognized
-    public AnimalDAOImpl() {
+    @Autowired
+    public AnimalDAOImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 }
